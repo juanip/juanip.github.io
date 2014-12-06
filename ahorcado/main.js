962d257af91df032e276120c35006e52c599f5ce
juanip <juani@paccapelo.com.ar>
2014-12-06 11:28:06 -0300

    agregado niveles, estadisticas, los datos locales se guardan en un json

--- ahorcado/main.js
+++ ahorcado/main.js
@@ -14,9 +14,36 @@ var Palabras = function(lista) {
 
 
 // Clase Jugador
-var Jugador = function(nombre_jugador, puntos) {
-  var nombre = nombre_jugador;
-  var puntos = puntos;
+var Jugador = function(datos) {
+  var nombre = datos['nombre_jugador'];
+  var nivel = parseInt(datos['nivel']);
+  var puntos = parseInt(datos['puntos']);
+  var partidas = parseInt(datos['partidas']);
+  var aciertos = parseInt(datos['aciertos']);
+  var errores = parseInt(datos['errores']);
+
+  var tabla_niveles = {
+    '1' : 100,
+    '2' : 250,
+    '3' : 365,
+    '4' : 574,
+    '5' : 892,
+    '6' : 1389,
+    '7' : 2162,
+    '8' : 3366,
+    '9' : 5240,
+    '10' : 8158,
+    '11' : 12701,
+    '12' : 19774,
+    '13' : 30786,
+    '14' : 47931,
+    '15' : 74624,
+    '16' : 116183,
+    '17' : 180887,
+    '18' : 281626,
+    '19' : 438468,
+    '20' : 682658
+  };
 
   this.get_puntos = function() {
     return puntos;
@@ -37,7 +64,52 @@ var Jugador = function(nombre_jugador, puntos) {
   this.set_nombre_jugador = function(nombre_jugador) {
     nombre = nombre_jugador;
   };
+
+  this.sumar_partida = function() {
+    partidas++;
+  };
+
+  this.get_partidas = function() {
+    return partidas;
+  };
+
+  this.sumar_acierto = function(cant) {
+    aciertos += cant;
+  };
+
+  this.get_aciertos = function() {
+    return aciertos;
+  };
   
+  this.sumar_error = function(cant) {
+    errores += cant;
+  };
+
+  this.get_errores = function() {
+    return errores;
+  };
+
+  this.set_nivel = function() {
+    if(this.get_puntos() > tabla_niveles[this.get_nivel()]) {
+      nivel++;
+    }
+  };
+
+  this.get_nivel = function() {
+    return nivel;
+  };
+
+  this.get_datos = function() {
+    datos = {};
+    datos['nombre_jugador'] = this.get_nombre_jugador();
+    datos['nivel'] = this.get_nivel();
+    datos['puntos'] = this.get_puntos();
+    datos['partidas'] = partidas;
+    datos['aciertos'] = aciertos;
+    datos['errores'] = errores;
+
+    return datos;
+  };
 }
 
 
@@ -49,8 +121,10 @@ var Juego = function(palabra) {
   var restantes = palabra_objetivo.length;
   var vidas = 6;
   var puntos = 0;
-  var puntos_sumar = 15;
-  var puntos_restar = 5;
+  var juego_finalizado = false;
+
+  var puntos_sumar = 12;
+  var puntos_restar = 11;
 
   for(cont = 0; cont < palabra_objetivo.length; cont++) {
     progreso += "_";
@@ -92,9 +166,14 @@ var Juego = function(palabra) {
   }
 
   this.probar_letra = function(letra) {
+    resultados = {
+        'aciertos' : 0,
+        'errores' : 0,
+    };
+    
     valida = letra.match(/[A-Z]/i) || letra == "ñ" ? true : false;
 
-    if(valida && intentos.indexOf(letra) == -1 && vidas > 0) {
+    if(valida && intentos.indexOf(letra) == -1 && vidas > 0 && restantes > 0 && !this.get_juego_finalizado()) {
       this.set_intentos(letra);
 
       exito = false;
@@ -104,23 +183,30 @@ var Juego = function(palabra) {
           restantes--;
           exito = true;
           this.sumar_puntos();
+          resultados['aciertos']++;
         }
       }
 
       if(!exito) {
         this.sub_vida();
         this.restar_puntos();
+        resultados['errores'] = 1;
       }
-
     }
+
+    return resultados;
+  };
+
+  this.get_juego_finalizado = function() {
+    return juego_finalizado;
   };
 
   this.terminar_juego = function() {
     progreso = palabra_objetivo;
     vidas = 0;
+    juego_finalizado = true;
     return puntos;
-  }
-  
+  }  
 }
 
 
@@ -135,12 +221,27 @@ function GUI(palabras, jugador) {
   var ctrl_letra = document.querySelector("#letra");
   var alert_victoria = document.querySelector("#victoria");
   var alert_derrota = document.querySelector("#derrota");
+  var puntos_victoria = document.querySelector("#puntos-victoria"); 
+  var puntos_derrota = document.querySelector("#puntos-derrota");
 
   var ctrl_nombre_usuario = document.querySelector("#nombre-usuario");
   ctrl_nombre_usuario.innerHTML = jugador.get_nombre_jugador();
 
+  var ctrl_nivel_usuario = document.querySelector("#nivel-usuario");
   var ctrl_puntos_usuario = document.querySelector("#puntos-usuario");
-  ctrl_puntos_usuario.innerHTML = jugador.get_puntos();
+  var ctrl_partidas_usuario = document.querySelector("#partidas-usuario");  
+  var ctrl_aciertos_usuario = document.querySelector("#aciertos-usuario");  
+  var ctrl_errores_usuario = document.querySelector("#errores-usuario");
+
+  this.actualizar_datos = function() {
+    ctrl_nivel_usuario.innerHTML = jugador.get_nivel();
+    ctrl_puntos_usuario.innerHTML = jugador.get_puntos();
+    ctrl_partidas_usuario.innerHTML = jugador.get_partidas();
+    ctrl_aciertos_usuario.innerHTML = jugador.get_aciertos();
+    ctrl_errores_usuario.innerHTML = jugador.get_errores();
+  };
+
+  this.actualizar_datos();
 
   var ctrl_usuario = document.querySelector("#usuario");
 
@@ -180,46 +281,57 @@ function GUI(palabras, jugador) {
 
     alert_victoria.style.display = "none";
     alert_derrota.style.display = "none";
+    document.querySelector('#alerta-nombre').style.display = "none";
+    document.querySelector('#alerta-puntos').style.display = "none";
 
     ctrl_letra.focus();
   };
 
-  this.terminar_juego = function(victoria) {
+  this.terminar_juego = function() {
     puntos = juego.terminar_juego();
     jugador.add_puntos(puntos);
+    jugador.sumar_partida();
+    jugador.set_nivel();
 
-    localStorage.setItem('puntos', jugador.get_puntos());
+    datos_jugador = jugador.get_datos();
 
-    ctrl_puntos_usuario.innerHTML = jugador.get_puntos();
+    localStorage.setItem('datos_jugador', JSON.stringify(datos_jugador));
 
-    if(victoria) {
+    this.actualizar_datos();
+
+    if(puntos > 0) {
+      puntos_victoria.innerHTML = puntos;
       alert_victoria.style.display = "block";
     }
     else {
+      puntos_derrota.innerHTML = puntos;
       alert_derrota.style.display = "block";
     }
   }
 
   this.jugar_letra = function() {
-    letra = ctrl_letra.value;
+    if(!juego.get_juego_finalizado()) {
+      letra = ctrl_letra.value;      
+  
+      resultados = juego.probar_letra(letra);
+  
+      jugador.sumar_error(resultados['errores']);
+      jugador.sumar_acierto(resultados['aciertos']);
+
+      this.dibujar_ahorcado(juego.get_vidas());
+  
+      if(juego.get_vidas() < 1 || juego.get_restantes() < 1) {
+        this.terminar_juego();
+      }
+      
+      this.set_intentos(juego.get_intentos());
+      this.set_progreso(juego.get_progreso());
+      
+    }
+
     ctrl_letra.value = "";
-
-    juego.probar_letra(letra);
-
-    this.dibujar_ahorcado(juego.get_vidas());
-
-    if(juego.get_vidas() < 1) {
-      this.terminar_juego(false);
-    }
-
-    if(juego.get_restantes() < 1) {
-      this.terminar_juego(true);
-    }
-    
-    this.set_intentos(juego.get_intentos());
-    this.set_progreso(juego.get_progreso());
-    
     ctrl_letra.focus();
+
   };
 
   this.pulsa_tecla = function(tecla) {
@@ -247,20 +359,34 @@ function GUI(palabras, jugador) {
     nombre = ctrl_usuario.value;
     if(nombre != '') {
       jugador.set_nombre_jugador(nombre);
+      datos_jugador = jugador.get_datos();
+      localStorage.setItem('datos_jugador', JSON.stringify(datos_jugador));
+
       ctrl_nombre_usuario.innerHTML = nombre;
-      localStorage.setItem('nombre-jugador', nombre);
-    }
+  };
 
     document.querySelector('.adm-usuario').style.display = "none";
     document.querySelector('#alerta-nombre').style.display = "block";
   };
 
-  this.borrar_puntos = function() {
-    jugador.borrar_puntos();
-    localStorage.setItem('puntos', 0);
-    ctrl_puntos_usuario.innerHTML = '0';
+  this.borrar_datos = function() {
+    datos_jugador = {
+      'nombre_jugador': 'Anonimo',
+      'nivel' : '1',
+      'puntos' : '0',
+      'partidas' : '0',
+      'aciertos' : '0',
+      'errores' : '0'
+    };
+    jugador = new Jugador(datos_jugador);
+
+    localStorage.setItem('datos_jugador', JSON.stringify(datos_jugador));
+
+    this.actualizar_datos();
     document.querySelector('#alerta-puntos').style.display = "block";
-  }
+  };
+
+  
 }
 
 var gui;
@@ -270,23 +396,23 @@ var gui;
 function init() {
   palabras = new Palabras(lista_palabras);
 
-  if(localStorage.getItem('nombre-jugador') != undefined) {
-    nombre_jugador = localStorage.getItem('nombre-jugador');
+  if(localStorage.getItem('datos_jugador') != undefined) {
+    datos_jugador = JSON.parse(localStorage.getItem('datos_jugador'));
   }
   else {
-    localStorage.setItem('nombre-jugador', 'Anonimo');
-    nombre_jugador = 'Anonimo';
+    datos_jugador = {
+      'nombre_jugador': 'Anonimo',
+      'nivel' : '1',
+      'puntos' : '0',
+      'partidas' : '0',
+      'aciertos' : '0',
+      'errores' : '0'
+    };
+
+    localStorage.setItem('datos_jugador', JSON.stringify(datos_jugador));
   }
 
-  if(localStorage.getItem('puntos') != undefined) {
-    puntos = parseInt(localStorage.getItem('puntos'));
-  }
-  else {
-    localStorage.setItem('puntos', '0');
-    puntos = 0;
-  }
-
-  jugador = new Jugador(nombre_jugador, puntos);
+  jugador = new Jugador(datos_jugador);
 
   gui = new GUI(palabras, jugador);
 
